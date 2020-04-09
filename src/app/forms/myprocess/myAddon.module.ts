c61ca92278fb7adbed242ad1599d3321b502bab5
cihataltuntas <btabcdef>
2020-04-09 12:11:48 +0300

    feat: satinalma component is added

--- src/app/forms/myprocess/myAddon.module.ts
+++ src/app/forms/myprocess/myAddon.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { startNewProcessComponent } from './startNewProcess.component';
 import { approveDataTaskComponent } from './approveDataTask.component';
 import { talepComponent } from './talep.component';
 import { hukukComponent } from './hukuk.component';
+import { satinalmaComponent } from './satinalma.component';
 
 
 @NgModule({
-  entryComponents: [startNewProcessComponent,approveDataTaskComponent, talepComponent, hukukComponent],
-  declarations: [startNewProcessComponent,approveDataTaskComponent, talepComponent, hukukComponent],
+  entryComponents: [startNewProcessComponent, approveDataTaskComponent, talepComponent, hukukComponent, satinalmaComponent],
+  declarations: [startNewProcessComponent, approveDataTaskComponent, talepComponent, hukukComponent, satinalmaComponent],
   imports: [FormsModule],
-  exports: [startNewProcessComponent,approveDataTaskComponent, talepComponent, hukukComponent]
+  exports: [startNewProcessComponent, approveDataTaskComponent, talepComponent, hukukComponent, satinalmaComponent]
 })
-export class MyAddonModule {}
+export class MyAddonModule { }
 
 export { startNewProcessComponent } from './startNewProcess.component';
 export { approveDataTaskComponent } from './approveDataTask.component';
 export { talepComponent } from './talep.component';
 export { hukukComponent } from './hukuk.component';
+export { satinalmaComponent } from './satinalma.component';
